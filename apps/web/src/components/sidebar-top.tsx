92ff257146a04cb83aae1c0121d41fee7b0ad671
axel husson <aaaxel.hsson@gmail.com>
2026-01-02 22:32:36 +0100

    clean sidebar top

--- apps/web/src/components/sidebar-top.tsx
+++ apps/web/src/components/sidebar-top.tsx
@@ -1,10 +1,4 @@
-import {
-  ChevronLeft,
-  ChevronRight,
-  LayoutGrid,
-  List,
-  SearchIcon,
-} from "lucide-react";
+import { LayoutGrid, List, SearchIcon } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { Link, useSearch, useNavigate } from "@tanstack/react-router";
 import { Route } from "@/routes/index";
@@ -20,17 +14,15 @@ export const SidebarTop = () => {
   const searchParams = useSearch({ from: Route.id });
   const navigate = useNavigate({ from: Route.id });
   return (
-    <div className="w-full h-[64px] bg-sidebar border-b border-border items-center px-8 flex flex-row gap-2">
-      <ChevronLeft className="w-6 h-6 opacity-40 cursor-pointer hover:opacity-100 transition-opacity" />
-      <ChevronRight className="w-6 h-6 opacity-40 cursor-pointer hover:opacity-100 transition-opacity" />
-      <div className="flex flex-row gap-1 rounded-lg bg-sidebar-accent p-1 items-center justify text-center">
+    <div className="w-full h-[64px] bg-sidebar border-b border-border items-center px-4 flex flex-row gap-8">
+      <div className="flex flex-row gap-1 rounded-lg bg-sidebar-accent/30 p-1 items-center justify text-center">
         <Link
           to="/"
           search={{ ...searchParams, view: "grid" }}
           className="p-1 rounded-md transition-all duration-300 opacity-40 hover:opacity-80"
           activeProps={{
             className:
-              "text-primary bg-blue-100/20 opacity-100 rounded-sm transition-all duration-300",
+              "text-accent-foreground opacity-100 bg-sidebar-accent/60 rounded-sm transition-all duration-300",
           }}
         >
           <LayoutGrid className="w-4 h-4" />
@@ -45,7 +37,7 @@ export const SidebarTop = () => {
           className="p-1 rounded-md transition-all duration-300 opacity-40 hover:opacity-80"
           activeProps={{
             className:
-              "text-primary bg-blue-100/20 opacity-100 rounded-sm transition-all duration-300",
+              "text-accent-foreground opacity-100 bg-sidebar-accent/60 rounded-sm transition-all duration-300",
           }}
         >
           <List className="w-4 h-4" />
@@ -68,9 +60,6 @@ export const SidebarTop = () => {
         />
         <InputGroupAddon>
           <SearchIcon />
-        </InputGroupAddon>
-        <InputGroupAddon align="inline-end">
-          <InputGroupButton>Search</InputGroupButton>
         </InputGroupAddon>
       </InputGroup>
     </div>
