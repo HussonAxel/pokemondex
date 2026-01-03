99005981b850aeca20ebc078775c88921cb29385
axel husson <aaaxel.hsson@gmail.com>
2026-01-03 13:39:09 +0100

    make use of first query

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,10 +1,11 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useLoaderData } from "@tanstack/react-router";
 import { SidebarTop } from "@/components/sidebar-top";
 import { SidebarBottom } from "@/components/sidebar-bottom";
 import { GridTemplateView } from "@/components/GridTemplateView";
 import { ListTemplateView } from "@/components/ListTemplateView.tsx";
 import { useSearch } from "@tanstack/react-router";
 import { z } from "zod";
+import { orpc } from "@/utils/orpc";
 
 export const Route = createFileRoute("/")({
   component: HomeComponent,
@@ -16,10 +17,21 @@ export const Route = createFileRoute("/")({
     catchedView: z.boolean().optional(),
     page: z.number().optional(),
   }),
+  loader: async ({ context }) => {
+    const pokemons = await context.queryClient.ensureQueryData(
+      orpc.getPokemons.queryOptions({
+        staleTime: 1000 * 60 * 60 * 24 * 30, // 30 days
+        gcTime: 1000 * 60 * 60 * 24 * 30, // 30 days
+      })
+    );
+    return { pokemons };
+  },
 });
 
 function HomeComponent() {
   const searchParams = useSearch({ from: Route.id });
+  const { pokemons } = useLoaderData({ from: Route.id });
+  console.log(pokemons);
   const isGrid = searchParams.view === "grid" || !searchParams.view;
 
   return (
