58b7db1618fd752771455209378950912b31e89d
axhus <axel.husson@smile.fr>
2026-01-08 15:04:43 +0100

    start the breeding tab

--- apps/web/src/components/sidebarRight/breeding.tsx
+++ apps/web/src/components/sidebarRight/breeding.tsx
@@ -1,4 +1,16 @@
+import { useSearch } from "@tanstack/react-router";
+import { Route } from "@/routes/index";
+import { orpc } from "@/utils/orpc";
+import { useQuery } from "@tanstack/react-query";
+
 export default function BreedingComponent() {
+  const searchParams = useSearch({ from: Route.id });
+  const activePokemon = searchParams.activePokemon;
+  const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${activePokemon}/`;
+  
+  const species = useQuery(orpc.getPokemonSpeciesData.queryOptions({ input: { url: pokemonSpeciesUrl } })).data;
+  if (!species) return null;
+
   return (
     <div>breeding</div>
   )
